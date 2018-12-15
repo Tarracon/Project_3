f3db721ea7942525ac3d44edc0cb15e9bba6bb3e
Tarracon <36968612+Tarracon@users.noreply.github.com>
2018-12-14 19:21:19 -0500

    Update project_3.s
    Added lines 22-30 which keep track of the spaces between letters

--- project_3.s
+++ project_3.s
@@ -20,4 +20,12 @@ main:
 		li $t1, 0 #Boolean
 		
 		li $t7, 0 #handles my array
+		addi $t2, $t2, 0 #counter for spaces in between letters
+		addi $t3, $t3, 0 #counter for letters
+		
+		addi $s0, $s0, 35 	#static base register 
+		addi $s1, $s1, 1 	#temp for powerStorage
+		
+		la $t4, userInputStorage
+		la $t5, powerStorage
 		
